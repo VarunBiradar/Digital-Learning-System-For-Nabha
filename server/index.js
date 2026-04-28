9ed18e6f1afa175309de8377451c7e0736991472
VarunBiradar <varunbiradar4300@gmail.com>
2026-04-28 19:20:44 +0530

    fix: allow Vercel frontend origin in CORS

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:4173',
+  'https://digital-learning-system-for-nabha.vercel.app',
   process.env.CLIENT_URL,
 ].filter(Boolean);
 
